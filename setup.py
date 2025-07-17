ee35065f4bcaaca8515fed0237a978901f06e7af
Jon Allen <joallen@nvidia.com>
2025-07-17 23:02:55 +0000

    Add build environment variables.

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import shlex
 import sys
 import warnings
 import os
@@ -16,6 +17,8 @@ from torch.utils.cpp_extension import (
     CUDA_HOME,
     load,
 )
+
+sys.argv.extend(shlex.split(os.environ.get("APEX_BUILD_ARGS", "")))
 
 # ninja build does not work unless include_dirs are abs path
 this_dir = os.path.dirname(os.path.abspath(__file__))
@@ -111,14 +114,15 @@ ext_modules = []
 
 extras = {}
 
-if "--cpp_ext" in sys.argv or "--cuda_ext" in sys.argv:
+if True:
     if TORCH_MAJOR == 0:
         raise RuntimeError(
             "--cpp_ext requires Pytorch 1.0 or later, " "found torch.__version__ = {}".format(torch.__version__)
         )
 
-if "--cpp_ext" in sys.argv:
-    sys.argv.remove("--cpp_ext")
+if True:
+    if "--cpp_ext" in sys.argv:
+        sys.argv.remove("--cpp_ext")
     ext_modules.append(CppExtension("apex_C", ["csrc/flatten_unflatten.cpp"]))
 
 
@@ -176,8 +180,9 @@ if "--distributed_lamb" in sys.argv:
         )
     )
 
-if "--cuda_ext" in sys.argv:
-    sys.argv.remove("--cuda_ext")
+if True:
+    if "--cuda_ext" in sys.argv:
+        sys.argv.remove("--cuda_ext")
     raise_if_cuda_home_none("--cuda_ext")
     check_cuda_torch_binary_vs_bare_metal(CUDA_HOME)
 
